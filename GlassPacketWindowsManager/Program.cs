b0d286e014a748a028b18a457eed5ed5ceb646de
IronCrocks <99284866+IronCrocks@users.noreply.github.com>
2023-12-26 12:20:45 +0300

    Add project

---
+++ GlassPacketWindowsManager/Program.cs
@@ -0,0 +1,34 @@
+﻿// See https://aka.ms/new-console-template for more information
+using GlassPacketWindowsManager;
+
+var articleNumbers = new List<string> {
+   "4/16/4",
+   "6 SG HD Silver Grey 32 ЗАК/16/6 м1/16/6 ЗАК",
+   "4 TOP-N/14/6_м1",
+   "4 ПлА2/16/4м1",
+   "4 TOP-N/14ar/4 м1/14ar/4_StClBr",
+   "10 м1/22/8 м1"};
+
+foreach (var item in articleNumbers)
+{
+    ShowArticle(item);
+}
+
+Console.WriteLine("Введите артикул:");
+Console.WriteLine();
+
+string? selectedArticle = Console.ReadLine();
+ShowArticle(selectedArticle!);
+
+Console.ReadLine();
+
+static void ShowArticle(string articleNumber)
+{
+    if (articleNumber is null) return;
+
+    var count = WindowService.GetSegmentsCount(articleNumber);
+    var windowThickness = WindowService.GetWindowThickness(articleNumber);
+    var glassThickness = WindowService.GetGlassThickness(articleNumber);
+
+    Console.WriteLine(articleNumber + " | " + count + " | " + windowThickness + " | " + glassThickness);
+}
---
+++ GlassPacketWindowsManager/WindowService.cs
@@ -0,0 +1,54 @@
+﻿using System.Text.RegularExpressions;
+
+namespace GlassPacketWindowsManager;
+
+public static class WindowService
+{
+    public static int GetSegmentsCount(string articleNumber)
+    {
+        var articleParts = articleNumber.Split("/");
+
+        return articleParts.Length switch
+        {
+            1 => 0,
+            3 => 1,
+            5 => 2,
+            _ => throw new InvalidOperationException("Не верный формат артикула."),
+        };
+    }
+
+    public static int GetWindowThickness(string articleNumber)
+    {
+        var windowParts = articleNumber.Split("/");
+        var windowPartsThickess = windowParts.Select(GetFirstNumbers).Select(int.Parse).ToList();
+
+        return windowPartsThickess.Sum();
+    }
+
+    public static int GetGlassThickness(string articleNumber)
+    {
+        var articleParts = articleNumber.Split("/");
+
+        return articleParts.Length switch
+        {
+            1 => int.Parse(GetFirstNumbers(articleParts.ElementAt(0))),
+            3 => GetSingleSegmentGlassThickness(articleParts),
+            5 => GetDoubleSegmentGlassThickness(articleParts),
+            _ => throw new InvalidOperationException("Не верный формат артикула."),
+        };
+    }
+
+    private static int GetDoubleSegmentGlassThickness(IEnumerable<string> windowParts)
+    {
+        var windowPartsThickess = windowParts.Select(GetFirstNumbers).Select(int.Parse).ToList();
+        return windowPartsThickess[0] + windowPartsThickess[2] + windowPartsThickess[4];
+    }
+
+    private static int GetSingleSegmentGlassThickness(IEnumerable<string> windowParts)
+    {
+        return int.Parse(GetFirstNumbers(windowParts.ElementAt(0))) + int.Parse(GetFirstNumbers(windowParts.ElementAt(2)));
+    }
+
+    private static string GetFirstNumbers(string str) => Regex.Match(str, @"\d+").Value;
+}
+
